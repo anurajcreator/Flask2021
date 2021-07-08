813d13ddc68f85b57148a1b77a968e3aa3fbd817
anurajcreator <anurajsingh883@gmail.com>
2021-07-08 14:45:07 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from re import template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route('/',methods=['GET','POST'])
+def data_entry():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template("index.html", allTodo=allTodo)
+    
+@app.route('/products')
+def products():
+    return 'This is Product Page'
+
+@app.route('/update/<int:sno>', methods=['GET','POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    update_ele = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', ele = update_ele)
+    
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    del_el = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(del_el)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__" :
+    app.run(debug=True)
+
